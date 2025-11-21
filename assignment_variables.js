412e2bced2f8c9d866efa5a5f7b5c1f8bd71c0e0
priyankakesasmetty <priyanka.kesamsetty@gmail.com>
2025-11-21 13:02:03 +0530

    first commit

---
+++ assignment_variables.js
@@ -0,0 +1,18 @@
+
+
+const browserVersion = "chrome";
+function getBrowserVersion(browserVersion) 
+{   
+  
+    if (browserVersion == "chrome") {
+      var browserVersion="4.0.7";
+      console.log("Launching Chrome browser","browserVersion");
+      console.log(browserVersion);
+}
+else 
+  {
+    console.log("Please provide a valid browser name")
+  }
+}
+console.log(browserVersion);
+getBrowserVersion("chrome");
---
+++ odd_even.js
@@ -0,0 +1,12 @@
+function isoddoreven(number)
+{
+    if(number % 2 == 0)
+    {
+        console.log(number + " is even");
+    }
+    else
+    {
+        console.log(number + " is odd");
+    }
+}    
+isoddoreven(7);
